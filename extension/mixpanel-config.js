59806dedc802ca62e9cced5788f56abeb5abc898
abhishekShah-000 <155077328+abhishekShah-000@users.noreply.github.com>
2024-12-25 16:53:47 +0530

    removed unused files

--- extension/mixpanel-config.js
+++
@@ -1,9 +0,0 @@
-// mixpanel-config.js
-const mixpanel = window.mixpanel;
-
-mixpanel.init('48a67766d0bb1b3399a4f956da9c52da', {
-    debug: true,
-    track_pageview: true
-});
-
-export default mixpanel;
